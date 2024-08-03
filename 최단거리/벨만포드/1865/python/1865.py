56d10891d2dfe6765e3407cb30e38ce0a3daece4
Hocheol <didifia9850@naver.com>
2024-08-03 16:25:14 +0900

    algo

--- 최단거리/벨만포드/1865/python/1865.py
+++ 최단거리/벨만포드/1865/python/1865.py
@@ -1,16 +1,42 @@
 import sys
 
+def bellman_ford(n, edges, start):
+    dis = [1e9] * (n + 1)
+    dis[start] = 0
+
+    # (n-1)번 반복하여 모든 간선을 완화
+    # 마지막 한 번 더 사이클을 돌면서 한 번 더 최소값이 갱신되면, 음수인 싸이클 존재
+    for i in range(n):
+        for u, v, w in edges:
+            if dis[u] + w < dis[v]:
+                dis[v] = dis[u] + w
+                if i == n-1:
+                    return False
+
+    # # 음수 사이클 체크: 출발점에서 도달할 수 있는 정점에서 음수 사이클 확인
+    # for u, v, w in edges:
+    #     if dis[u] + w < dis[v]:
+    #         return False  # 음수 사이클 존재
+
+    return True
+
 tc = int(sys.stdin.readline())
 for _ in range(tc):
     n, m, w = map(int, sys.stdin.readline().split())
-    graph = [[] for _ in range(n+1)]
-    graph_2 = [[] for _ in range(n+1)]
-    for _ in range(m+w):
+    edges = []
+    for _ in range(m):
         s, e, t = map(int, sys.stdin.readline().split())
-        graph[s].append([s,e,t])
-        graph[e].append([e,s,t])
+        edges.append((s, e, t))
+        edges.append((e, s, t))
+    for _ in range(w):    
         ms, me, mt = map(int, sys.stdin.readline().split())
-        graph_2[ms].append([ms, me, mt])
+        edges.append((ms, me, -abs(mt)))
 
-    print(graph)
-    print(graph_2)
+    has_negative_cycle = False
+    if not bellman_ford(n, edges, 1):
+        has_negative_cycle = True
+        
+    if has_negative_cycle:
+        print("YES")
+    else:
+        print("NO")
