e0db31bad79b4b5ee6c5653ef59037e80ce4f307
Hocheol Park <didifia9850@naver.com>
2024-08-16 00:09:14 +0900

    Algo

--- 구현/아날로그 시계/python/아날로그 시계.py
+++ 구현/아날로그 시계/python/아날로그 시계.py
@@ -1,12 +1,8 @@
-# 0.00833333 도
-# 0.1 도
-# 6 도
-
 def solution(h1, m1, s1, h2, m2, s2):
     now_time = h2 * 3600 + m2 * 60 + s2
     answer = 0
     ph, pm, ps = h1 % 12 * 3600 * 0.0083333 + m1 * 60 * 0.0083333 + s1 * 0.0083333, m1 * 60 * 0.1 + s1 * 0.1, s1 * 6
-    print(ph, pm, ps)
+    
     for time_ in range(h1 * 3600 + m1 * 60 + s1 , h2 * 3600 + m2 * 60 + s2 + 1):
 
         h, m, s = time_ // 3600, time_ % 3600 // 60, time_ % 3600 % 60
@@ -16,7 +12,8 @@ def solution(h1, m1, s1, h2, m2, s2):
 
         if time == 0: time = 360
         if minute == 0: minute = 360
-        if second == 0: second = 360
+        if h1 * 3600 + m1 * 60 + s1 != time_:
+            if second == 0: second = 360
         
         if time_ == 43200 or time_ == 0:
             answer += 1 
@@ -24,14 +21,12 @@ def solution(h1, m1, s1, h2, m2, s2):
             if second >= minute:
                 if pm > ps:
                     answer += 1
-                   
+                
             if second >= time:
                 if ph > ps:
                     answer += 1
-                   
+                
         ph, pm, ps = time, minute, second
-
-        print(time, minute, second, answer)
 
         if ph == 360: ph = 0
         if pm == 360: pm = 0
@@ -41,34 +36,3 @@ def solution(h1, m1, s1, h2, m2, s2):
             break
 
     return answer
-
-print(solution(0,59,0,1,0,1))
-
-def solution2(h1, m1, s1, h2, m2, s2):
-    def get_times(h, m, s):
-        seconds = h * 60 * 60 + m * 60 + s
-
-        sd = (s * 6) % 360
-        md = (m * 6 + s * 0.1) % 360
-        hd = (h * 30 + m * 0.5 + s * 0.5 / 60) % 360
-
-        ret = (h * 60 + m) * 2 - h
-
-        if sd >= md:
-            ret += 1
-        if sd >= hd:
-            ret += 1
-
-        if h >= 12:
-            ret -= 2
-
-        return ret
-
-    ret = get_times(h2, m2, s2) - get_times(h1, m1, s1)
-
-    if h1 in [0, 12] and m1 == 0 and s1 == 0:
-        ret += 1
-
-    return ret
-
-print(solution2(0,59,0,1,0,1))
