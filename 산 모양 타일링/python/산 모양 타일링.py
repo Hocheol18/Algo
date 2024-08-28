69d8170868015995e135f2baf7ca7cc494937925
Hocheol Park <didifia9850@naver.com>
2024-08-28 23:47:36 +0900

    Algo

---
+++ 산 모양 타일링/python/산 모양 타일링.py
@@ -0,0 +1,11 @@
+from itertools import combinations
+
+def solution(n, tops):
+    answer = 0
+
+    for i in range(1, n+1):
+        print(i)
+
+    return answer
+
+solution(2, [0, 1])
--- 에어컨/python/에어컨.py
+++ 에어컨/python/에어컨.py
@@ -1,4 +1,9 @@
 def solution(temperature, t1, t2, a, b, onboard):
+    
+    for ele in onboard:
+        if ele == 0:
+            pass
+
     answer = 0
     answer += 1
     return answer
