04ed4ab477613d02ad472b86048da62f3f2708bc
Hocheol Park <didifia9850@naver.com>
2024-08-29 22:13:36 +0900

    Algo

--- 에어컨/python/에어컨.py
+++ 에어컨/python/에어컨.py
@@ -3,9 +3,11 @@ def solution(temperature, t1, t2, a, b, onboard):
     for ele in onboard:
         if ele == 0:
             pass
-
+            
+    
     answer = 0
     answer += 1
+    answer -= 1
     return answer
 
 solution(28,18,	26,	10,	8,	[0, 0, 1, 1, 1, 1, 1])
