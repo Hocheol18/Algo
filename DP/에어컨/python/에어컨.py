8577eb1b2de9341beb5458761f60e246cdb712b3
Hocheol Park <didifia9850@naver.com>
2024-09-25 02:24:21 +0900

    Algo

--- DP/에어컨/python/에어컨.py
+++ DP/에어컨/python/에어컨.py
@@ -1,78 +1,22 @@
-from collections import deque
-
 def solution(temperature, t1, t2, a, b, onboard):
+    dp = [[1e9 for _ in range(80)] for _ in range(len(onboard))]
+    dp[0][temperature + 15] = 0
+
+    for i in range(1, len(onboard)):
+        start, end = 0, 0
     
-    queue = deque()
-    queue.append([0, temperature, 0])
+        if onboard[i]:
+            start, end = t1 + 15, t2 + 15
 
-    while queue:
-        index, nowtem, answer = queue.popleft()
+        else:
+            start, end = min(t1, temperature) + 15, max(t2, temperature) + 15
 
-        if (onboard[index] == 0 or (onboard[index] == 0 and (nowtem > t2 or nowtem < t1))) and index == len(onboard) - 1:
-            return answer
-
-        if index == len(onboard) - 1:
-            continue
-        
-        if onboard[index] == 1 and (nowtem > t2 or nowtem < t1):
-            continue
-            
-      
-        # 세 케이스
-        # 끄는 경우, 유지하는 경우, 에어컨 가동하는 경우
-
-        # 다음턴이 특정 범위 안에 온도가 있어야 한다면
-        if onboard[index+1] == 1:
-            # 세 가지 케이스 전부 적용
-            if t1 < nowtem < t2:
-                queue.append([index+1, nowtem + 1, answer + a])
-                queue.append([index+1, nowtem - 1, answer + a])
-                queue.append([index+1, nowtem, answer + b])
-                queue.append([index+1, nowtem, answer + b])
-                queue.append([index+1, nowtem + 1, answer])
-                queue.append([index+1, nowtem - 1, answer])
-
-            elif nowtem > t2:
-                queue.append([index+1, nowtem - 1, answer + a])
-
-            elif nowtem < t1:
-                queue.append([index+1, nowtem + 1, answer + a])
-
-            elif nowtem == t2:
-                queue.append([index+1, nowtem - 1, answer + a ])
-                queue.append([index+1, nowtem, answer + b])
-
-            elif nowtem == t1:
-                queue.append([index + 1, nowtem + 1, answer + a])
-                queue.append([index+1, nowtem, answer + b])
-
-        elif onboard[index+1] == 0:
-            if t1 < nowtem < t2:
-                queue.append([index+1, nowtem + 1, answer + a])
-                queue.append([index+1, nowtem - 1, answer + a])
-                queue.append([index+1, nowtem, answer + b])
-                queue.append([index+1, nowtem, answer + b])
+        for j in range(start, end + 1):
+            if temperature + 15 > j:
+                dp[i][j] = min( dp[i-1][j] + b, dp[i-1][j-1] + 0, dp[i-1][j+1] + a)
+            elif temperature + 15 < j:
+                dp[i][j] = min(dp[i-1][j-1] + a, dp[i-1][j] + b, dp[i-1][j+1] + 0)
+            else:
+                dp[i][j] = min(dp[i-1][j-1] + a, dp[i-1][j] + 0, dp[i-1][j+1] + a)
                 
-            elif nowtem > t2:
-                queue.append([index+1, nowtem - 1, answer + a])
-                queue.append([index + 1, nowtem, answer + b])
-             
-
-            elif nowtem < t1:
-                queue.append([index+1, nowtem + 1, answer + a])
-                queue.append([index + 1, nowtem, answer + b])
-             
-
-            elif nowtem == t2:
-                queue.append([index+1, nowtem - 1, answer + a ])
-                queue.append([index+1, nowtem, answer + b])
-          
-
-            elif nowtem == t1:
-                queue.append([index + 1, nowtem + 1, answer + a])
-                queue.append([index+1, nowtem, answer + b])
-
-            queue.append([index+1, nowtem + 1, answer])
-            queue.append([index+1, nowtem - 1, answer])
-
-print(solution(28,	18,	26,	10,	8,	[0, 0, 1, 1, 1, 1, 1]))
+    return min(dp[-1])
